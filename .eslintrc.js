a6cd9dacb2d94f9b3ddfd1c0e785571c5bdc3c3f
GeorgeH <default_hyn@163.com>
2023-02-14 01:12:41 +0800

    test1

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,9 +7,7 @@ module.exports = {
     "plugin:vue/vue3-essential",
     "eslint:recommended",
     "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint",
-    'plugin:prettier/recommended'
+    "plugin:prettier/recommended"
   ],
   parserOptions: {
     ecmaVersion: 2020
