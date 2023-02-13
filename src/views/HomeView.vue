40e575a50a24dd5ae07cd77dbf56efc2207db34b
GeorgeH <default_hyn@163.com>
2023-02-14 01:03:16 +0800

    test1

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,17 +6,12 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue"
+import { defineComponent } from "vue";
 import HelloWorld from "@/components/HelloWorld.vue" // @ is an alias to /src
 
 export default defineComponent({
   name: 'HomeView',
   components: {
     HelloWorld,
-
-
-
-
-    
 })
 </script>
