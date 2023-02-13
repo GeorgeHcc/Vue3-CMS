d549ee995133cca5ba67369a35ce96117847047b
GeorgeH <default_hyn@163.com>
2023-02-14 01:55:31 +0800

    feat(global): update commit normalize

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,7 +8,7 @@
 <script lang="ts">
 import { defineComponent } from "vue"
 import HelloWorld from "@/components/HelloWorld.vue" // @ is an alias to /src
-
+//ttt
 export default defineComponent({
   name: "HomeView",
   components: {
