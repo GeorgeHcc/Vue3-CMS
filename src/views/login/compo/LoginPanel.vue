cc4e2e486bd776317c6ff9b102aa96f9d8f1200b
GeorgeH <default_hyn@163.com>
2023-03-06 21:45:24 +0800

    feat: config lint🚀🚀🚀

--- src/views/login/compo/LoginPanel.vue
+++ src/views/login/compo/LoginPanel.vue
@@ -3,14 +3,16 @@
 </template>
 
 <script lang="ts">
+
 import { defineComponent } from "vue"
 
 export default defineComponent({
   setup() {
     return {}
   }
-  
 })
 </script>
 
-<style scoped></style>
+<style scoped>
+
+</style>
